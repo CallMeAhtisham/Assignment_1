16acae819a56525545dc7c689b1bc2cb73b7db29
Ahtisham <iamahtishamahmedx@gmail.com>
2023-09-23 17:31:17 +0500

    s

--- 22.ts
+++ 22.ts
@@ -2,3 +2,8 @@
 one happen. Change an index in one of your programs to produce an index error. Make sure you correct the
 error before closing the program. */
 
+let new_fuits = ["Orange", "Banana", "Grapes", "Mango", "Apple"]
+
+console.log(`Index Error: ${new_fuits[5]}`)
+
+console.log(`Correction of Error: ${new_fuits[4]}`)
---
+++ 23.ts
@@ -0,0 +1,54 @@
+/* Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your
+prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+
+let car = "Subaru"
+
+console.log("Is car == 'Subaru'? I predict True.")
+
+console.log(car == "Subaru")
+
+console.log("Is car === 'Subaru'? I predict True.")
+
+console.log(car === "Subaru")
+
+console.log("Is car != 'Nissan'? I predict True.")
+
+console.log(car != "Nissan")
+
+console.log("Is car !== 'Nissan'? I predict True.")
+
+console.log(car !== "Nissan")
+
+console.log("Is car != 'Bugatti'? I predict True.")
+
+console.log(car != "Bugatti")
+
+console.log("Is car == 'Nissan'? I predict False.")
+
+console.log(car == "Nissan")
+
+console.log("Is car === 'Nissan'? I predict False.")
+
+console.log(car === "Nissan")
+
+console.log("Is car != 'Subaru'? I predict False.")
+
+console.log(car != "Subaru")
+
+console.log("Is car !== 'Subaru'? I predict Fasle.")
+
+console.log(car !== "Subaru")
+
+console.log("Is car == 'Nissan'? I predict False.")
+
+console.log(car == "Nissan")
