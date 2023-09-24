7bcbdcd7346a3b38448a843a744e708ec2272af8
Ahtisham <iamahtishamahmedx@gmail.com>
2023-09-24 16:20:08 +0500

    s

--- 40.ts
+++ 40.ts
@@ -6,3 +6,31 @@ make_album() that allows you to store the number of tracks on an album. If the c
 value for the number of tracks, add that value to the album’s Object. Make at least one new function call
 that includes the number of tracks on an album. */
 
+type Album = {
+     artist_name: string
+     album_title: string
+     tracks?: number
+}
+
+function make_album(artist_name: string, album_title: string, tracks?: number) {
+let album: Album = {
+     artist_name,
+     album_title
+}
+
+if (tracks) {
+     album.tracks = tracks
+}
+
+return album
+}
+
+let album1 = make_album("Artist 1", "Album 1")
+let album2 = make_album("Artsit 2", "Album 2")
+let album3 = make_album("Artist 3", "Album 3", 21)
+
+console.log(album1)
+
+console.log(album2)
+
+console.log(album3)
